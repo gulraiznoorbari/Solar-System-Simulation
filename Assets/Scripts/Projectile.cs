d1b320590c9ca20cad33b38ebe23a53c5a693da5
gulraiz <gulraiznoorbari@gmail.com>
2023-08-29 23:51:26 +0500

    Added project files

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Tooltip("The distance this projectile will move each second in meters.")]
+    [SerializeField] private float projectileSpeed = 3.0f;
+    [Tooltip("How far away from the main camera before destroying the projectile gameobject in meters.")]
+    [SerializeField] private float destroyDistance = 20.0f;
+
+    private void Update()
+    {
+        MoveProjectile();
+    }
+
+    /// Move the projectile in the direction it is heading
+    private void MoveProjectile()
+    {
+        // move the transform
+        transform.position = transform.position + transform.forward * projectileSpeed * Time.deltaTime;
+        // calculate the distance from the main camera
+        float dist = Vector3.Distance(Camera.main.transform.position, transform.position);
+        // if the distance is greater than the destroyDistance
+        if (dist > destroyDistance)
+        {
+            Destroy(this.gameObject); // destroy the gameObject
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject spawnPrefab = null;
+    [SerializeField] private float spawnTime = 5.0f;
+
+    // keep track of time passed for next spawn
+    private float nextSpawn = 0f;
+
+    private void Start()
+    {
+        nextSpawn = 0f;
+    }
+
+    private void Update()
+    {
+        // update the time until nextSpawn
+        nextSpawn += Time.deltaTime;
+        if (nextSpawn > spawnTime)
+        {
+            // Spawn the gameObject at the spawners current position and rotation
+            GameObject projectileGameObject = Instantiate(spawnPrefab, transform.position, transform.rotation, null);
+            // reset the time until nextSpawn
+            nextSpawn = 0f;
+        }
+
+    }
+}
